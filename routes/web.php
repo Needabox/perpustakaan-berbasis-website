ddbf08f1ecee4dccb087329b279e7a79b505eb24
needaboxx <needaboxx@gmail.com>
2024-01-01 19:12:52 +0700

    Move route

--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('category', CategoryController::class);
+    
+    Route::resource('user', UserController::class);
+    
+    Route::resource('category', CategoryController::class);
+    Route::resource('book', BookController::class);
 });
 
-Route::resource('user', UserController::class);
-
-Route::resource('category', CategoryController::class);
-Route::resource('book', BookController::class);
 
 require __DIR__.'/auth.php';
